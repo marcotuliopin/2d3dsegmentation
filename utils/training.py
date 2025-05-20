3b4ca7edb74d4fad2dca6dc26e9893979e903774
Marco Túlio P. T. Tristão <marcotuliopin@gmail.com>
2025-05-19 21:13:52 -0300

    Add ignore_index to focal loss

--- utils/training.py
+++ utils/training.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from tqdm import tqdm
 
+
 class CheckpointSaver:
     def __init__(self, model, optimizer, save_dir):
         self.model = model
@@ -94,13 +95,17 @@ class SegmentationTrainer:
 
 
 class FocalLoss(nn.Module):
-    def __init__(self, alpha=0.25, gamma=2.0):
+    def __init__(self, alpha=0.25, gamma=2.0, ignore_index=None):
         super().__init__()
         self.alpha = alpha  # Balances positive/negative
         self.gamma = gamma  # Focuses on hard examples
+        self.ignore_index = ignore_index
 
     def forward(self, inputs, targets):
-        ce_loss = F.cross_entropy(inputs, targets, reduction='none')
+        if self.ignore_index is not None:
+            ce_loss = F.cross_entropy(inputs, targets, reduction='none', ignore_index=self.ignore_index)
+        else:
+            ce_loss = F.cross_entropy(inputs, targets, reduction='none')
         pt = torch.exp(-ce_loss)  # Softmax probability of true class
         focal_loss = (self.alpha * (1-pt)**self.gamma * ce_loss)
         return focal_loss.mean()
