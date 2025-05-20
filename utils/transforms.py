e245a4bf95a979a15590a60877bc85581ab3aa73
Marco Túlio P. T. Tristão <marcotuliopin@gmail.com>
2025-05-19 21:17:02 -0300

    add more intensity to data augmentations

--- utils/transforms.py
+++ utils/transforms.py
@@ -2,14 +2,15 @@ import cv2
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
+# TODO: Run without data augmentation
 def get_training_transforms(height=256, width=352):
     return A.Compose([
-        A.Resize(height=height, width=width, interpolation=cv2.INTER_LINEAR),
-        A.HorizontalFlip(p=0.3),
+        A.RandomResizedCrop(size=(height, width), scale=(0.8, 1.0), ratio=(0.9, 1.1)),
+        A.HorizontalFlip(p=0.4),
         A.GridDistortion(p=0.4),
-        A.RandomBrightnessContrast(p=0.2),
-        A.ColorJitter(p=0.2),
-        A.GaussNoise(p=0.4),
+        A.RandomBrightnessContrast(p=0.4),
+        A.ColorJitter(p=0.4),
+        A.GaussNoise(p=0.2),
         A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
         ToTensorV2(),
     ], additional_targets={'mask': 'mask'})
