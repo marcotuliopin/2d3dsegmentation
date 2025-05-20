ac20546c0a2b3be68739c74bc3a6f85e5c7047c1
Marco Túlio P. T. Tristão <marcotuliopin@gmail.com>
2025-05-19 23:04:38 -0300

    fix data installation script

--- install_nyu_data.sh
+++ install_nyu_data.sh
@@ -5,6 +5,6 @@ mkdir -p data/NYUDepthv2_seg
 git clone --depth 1 https://huggingface.co/datasets/wyrx/NYUDepthv2_seg data/NYUDepthv2_seg
 rm -rf data/NYUDepthv2_seg/.git data/NYUDepthv2_seg/.gitattributes
 
-mv data/NYUDepthv2_seg/train-00000-of-00001.parquet data/NYUDepthv2_seg/train.parquet
-mv data/NYUDepthv2_seg/test-00000-of-00001.parquet data/NYUDepthv2_seg/test.parquet
+mv data/NYUDepthv2_seg/data/train-00000-of-00001.parquet data/NYUDepthv2_seg/data/train.parquet
+mv data/NYUDepthv2_seg/data/test-00000-of-00001.parquet data/NYUDepthv2_seg/data/test.parquet
 
