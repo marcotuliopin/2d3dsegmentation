b300643c6e5bd2c750e0fd1e2b763514f3521750
Marco Túlio P. T. Tristão <marcotuliopin@gmail.com>
2025-05-24 14:48:34 -0300

    modify transform

--- utils/transforms.py
+++ utils/transforms.py
@@ -3,19 +3,18 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 # TODO: Run without data augmentation
-def get_training_transforms(height=256, width=352):
+def get_training_transforms(height, width):
     return A.Compose([
         A.RandomResizedCrop(size=(height, width), scale=(0.8, 1.0), ratio=(0.9, 1.1)),
-        A.HorizontalFlip(p=0.4),
+        A.HorizontalFlip(p=0.5),
         A.GridDistortion(p=0.4),
         A.RandomBrightnessContrast(p=0.4),
         A.ColorJitter(p=0.4),
-        A.GaussNoise(p=0.2),
         A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
         ToTensorV2(),
     ], additional_targets={'mask': 'mask'})
 
-def get_validation_transforms(height=256, width=352):
+def get_validation_transforms(height, width):
     return A.Compose([
         A.Resize(height=height, width=width, interpolation=cv2.INTER_NEAREST),
         A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
