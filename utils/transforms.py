2bf194fcb093108d6f1d4a490ed8750c1cd3a9b1
Marco Túlio P. T. Tristão <marcotuliopin@gmail.com>
2025-05-25 20:48:11 -0300

    add better performant transforms

--- utils/transforms.py
+++ utils/transforms.py
@@ -2,21 +2,21 @@ import cv2
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
-# TODO: Run without data augmentation
+
 def get_training_transforms(height, width):
     return A.Compose([
-        A.RandomResizedCrop(size=(height, width), scale=(0.8, 1.0), ratio=(0.9, 1.1)),
+        A.Resize(height=height, width=width, interpolation=cv2.INTER_LINEAR),
         A.HorizontalFlip(p=0.5),
-        A.GridDistortion(p=0.4),
-        A.RandomBrightnessContrast(p=0.4),
+        A.Rotate(limit=10, p=0.5),
         A.ColorJitter(p=0.4),
+        A.RandomBrightnessContrast(p=0.3),
         A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
         ToTensorV2(),
     ], additional_targets={'mask': 'mask'})
 
 def get_validation_transforms(height, width):
     return A.Compose([
-        A.Resize(height=height, width=width, interpolation=cv2.INTER_NEAREST),
+        A.Resize(height=height, width=width, interpolation=cv2.INTER_LINEAR),
         A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
         ToTensorV2(),
     ], additional_targets={'mask': 'mask'})
