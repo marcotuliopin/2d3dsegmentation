c4b56f937c2809d3881190ae1259989aa6023a4e
Marco Túlio P. T. Tristão <marcotuliopin@gmail.com>
2025-05-25 21:06:33 -0300

    update unet code

--- models/unet.py
+++ models/unet.py
@@ -2,11 +2,14 @@ import torch.nn as nn
 import segmentation_models_pytorch as smp
 
 class UNet(nn.Module):
-    def __init__(self, num_classes, encoder="resnet50", dropout=0.2, in_channels=3, freeze_backbone=False, pretrained=True):
+    def __init__(self, num_classes, encoder="resnet50", dropout=0.3, in_channels=3, freeze_backbone=False, pretrained=True):
         super().__init__()
+
+        weights = "imagenet" if pretrained else None
+
         self.unet = smp.Unet(
             encoder_name=encoder,
-            encoder_weights="imagenet" if pretrained else None,
+            encoder_weights=weights,
             in_channels=in_channels,
             classes=num_classes,
             activation=None
@@ -15,11 +18,15 @@ class UNet(nn.Module):
         # Add dropout in all decoder blocks
         for i in range(len(self.unet.decoder.blocks)):
             self.unet.decoder.blocks[i].dropout = nn.Dropout2d(p=dropout)
+        
+        if freeze_backbone:
+            for param in self.unet.encoder.parameters():
+                param.requires_grad = False
 
     def forward(self, x):
         return self.unet(x)
 
 
-def get_unet_resnet50(num_classes, dropout=0.2, in_channels=3, freeze_backbone=False, pretrained=True, encoder="resnet50"):
-    print("Using model: UNet ResNet50")
+def get_unet_resnet(num_classes, dropout=0.3, in_channels=3, freeze_backbone=False, pretrained=True, encoder="resnet50"):
+    print(f"Using model: UNet with {encoder} encoder")
     return UNet(num_classes=num_classes, dropout=dropout, in_channels=in_channels, freeze_backbone=freeze_backbone, pretrained=pretrained, encoder=encoder)
