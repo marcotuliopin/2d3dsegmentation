89514356c915b48ae6ab879e0a87d3db786da828
Marco Túlio P. T. Tristão <marcotuliopin@gmail.com>
2025-06-07 15:56:25 -0300

    fix env

--- experiments/experiment1.sh
+++ experiments/experiment1.sh
@@ -3,14 +3,23 @@
 PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$PROJECT_ROOT"
 
-python train.py -n unet_resnet50_2enc \
+python train.py -n unet_resnet50_2enc_hha \
     --optimizer sgd \
     --scheduler step \
     --loss weighted_cross_entropy \
-    --model unet_dual_encoder \
+    --model unet_hha_dual_encoder \
 
 echo "Evaluating experiment..."
-python evaluate.py -n unet_resnet50_2enc
+python evaluate.py -n unet_resnet50_2enc_hha
+
+python train.py -n unet_resnet50_2enc_d \
+    --optimizer sgd \
+    --scheduler step \
+    --loss weighted_cross_entropy \
+    --model unet_depth_dual_encoder \
+
+echo "Evaluating experiment..."
+python evaluate.py -n unet_resnet50_2enc_d
 
 python train.py -n unet_resnet50_hha_1 \
     --optimizer sgd \
@@ -20,6 +29,15 @@ python train.py -n unet_resnet50_hha_1 \
 
 echo "Evaluating experiment..."
 python evaluate.py -n unet_resnet50_hha_1
+
+python train.py -n unet_resnet50_hha_fl \
+    --optimizer sgd \
+    --scheduler step \
+    --loss focal_loss \
+    --model unet_hha_concatenate \
+
+echo "Evaluating experiment..."
+python evaluate.py -n unet_resnet50_hha_fl
 
 echo "unet_resnet50_w experiment started..."
 python train.py -n unet_resnet50_w_1 \
