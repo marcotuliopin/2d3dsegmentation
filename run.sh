27e5e3f3284d909baaaa581f944886d0e81f39b7
Marco Túlio P. T. Tristão <marcotuliopin@gmail.com>
2025-06-20 15:36:46 -0300

    fix(run): update training and evaluation scripts for early and late fusion models

--- run.sh
+++ run.sh
@@ -1,71 +1,71 @@
 #!/bin/bash
 
 
-python train.py -n unet_early_fusion_hha_2006 \
+python train.py -n early_fusion_hha_att_2006 \
+    --optimizer sgd \
+    --scheduler step \
+    --loss weighted_cross_entropy \
+    --model early_fusion_hha_att \
+
+echo "Evaluating experiment..."
+python evaluate.py -n early_fusion_hha_att_2006
+
+
+python train.py -n late_fusion_hha_att_2006 \
+    --optimizer sgd \
+    --scheduler step \
+    --loss weighted_cross_entropy \
+    --model late_fusion_hha_att \
+
+echo "Evaluating experiment..."
+python evaluate.py -n late_fusion_hha_att_2006
+
+
+python train.py -n late_fusion_d_2006 \
+    --optimizer sgd \
+    --scheduler step \
+    --loss weighted_cross_entropy \
+    --model late_fusion_d \
+
+echo "Evaluating experiment..."
+python evaluate.py -n late_fusion_d_2006
+
+
+python train.py -n late_fusion_hha_2006 \
+    --optimizer sgd \
+    --scheduler step \
+    --loss weighted_cross_entropy \
+    --model late_fusion_hha \
+
+echo "Evaluating experiment..."
+python evaluate.py -n late_fusion_hha_2006
+
+
+python train.py -n early_fusion_hha_2006 \
+    --optimizer sgd \
+    --scheduler step \
+    --loss weighted_cross_entropy \
+    --model early_fusion_hha \
+
+echo "Evaluating experiment..."
+python evaluate.py -n early_fusion_hha_2006
+
+
+python train.py -n early_fusion_d_2006 \
     --optimizer sgd \
     --scheduler step \
     --loss weighted_cross_entropy \
     --model early_fusion_d \
 
 echo "Evaluating experiment..."
-python evaluate.py -n unet_early_fusion_hha_2006
+python evaluate.py -n early_fusion_d_2006
 
 
-# python train.py -n unet_hha_2enc_1806 \
-#     --optimizer sgd \
-#     --scheduler step \
-#     --loss weighted_cross_entropy \
-#     --model unet_hha_dual_encoder \
+python train.py -n rgb_only_2006 \
+    --optimizer sgd \
+    --scheduler step \
+    --loss weighted_cross_entropy \
+    --model rgb_only \
 
-# echo "Evaluating experiment..."
-# python evaluate.py -n unet_hha_2enc_1806
-
-
-# python train.py -n unet_depth_2enc_1806 \
-#     --optimizer sgd \
-#     --scheduler step \
-#     --loss weighted_cross_entropy \
-#     --model unet_depth_dual_encoder \
-
-# echo "Evaluating experiment..."
-# python evaluate.py -n unet_depth_2enc_1806
-
-
-# python train.py -n unet_depth_2enc_1806 \
-#     --optimizer sgd \
-#     --scheduler step \
-#     --loss weighted_cross_entropy \
-#     --model unet_depth_dual_encoder \
-
-# echo "Evaluating experiment..."
-# python evaluate.py -n unet_depth_2enc_1806
-
-
-# python train.py -n unet_hha_concatenate_1806 \
-#     --optimizer sgd \
-#     --scheduler step \
-#     --loss weighted_cross_entropy \
-#     --model unet_hha_concatenate \
-
-# echo "Evaluating experiment..."
-# python evaluate.py -n unet_hha_concatenate_1806
-
-
-# python train.py -n unet_depth_concatenate_1806 \
-#     --optimizer sgd \
-#     --scheduler step \
-#     --loss weighted_cross_entropy \
-#     --model unet_depth_concatenate \
-
-# echo "Evaluating experiment..."
-# python evaluate.py -n unet_depth_concatenate_1806
-
-
-# python train.py -n unet_1806 \
-#     --optimizer sgd \
-#     --scheduler step \
-#     --loss weighted_cross_entropy \
-#     --model unet \
-
-# echo "Evaluating experiment..."
-# python evaluate.py -n unet_1806
+echo "Evaluating experiment..."
+python evaluate.py -n rgb_only_2006
