d9c8c80327ef7f0297b7d5cd3ab43559d055d775
Marco Túlio P. T. Tristão <marcotuliopin@gmail.com>
2025-06-19 23:57:48 -0300

    fix(models): refactor fusion layer in UnetMidFusionHHA to use direct connections and remove unnecessary layers

--- models/unet_mid_fusion_hha.py
+++ models/unet_mid_fusion_hha.py
@@ -34,14 +34,11 @@ class UnetMidFusionHHA(nn.Module):
 
         self.fusion_encoder = Encoder(dropout=dropout)
 
-        self.fusion_layers = nn.ModuleList([nn.Conv2d(in_channels=ch * 3, out_channels=ch, kernel_size=1)
-                                            for ch in self.fusion_encoder.out_channels[1:]])
-        
         self.rgb_norms = nn.ModuleList([nn.BatchNorm2d(ch) for ch in self.rgb_encoder.out_channels])
         self.hha_norms = nn.ModuleList([nn.BatchNorm2d(ch) for ch in self.hha_encoder.out_channels])
 
         self.decoder = UnetDecoder(
-            encoder_channels=self.fusion_encoder.out_channels,
+            encoder_channels=self.rgb_encoder.out_channels,
             decoder_channels=(512, 256, 128, 64, 32),
             n_blocks=5,
         )
@@ -59,24 +56,23 @@ class UnetMidFusionHHA(nn.Module):
         rgb_feats_norm = [norm(feat) for feat, norm in zip(rgb_feats, self.rgb_norms)]
         hha_feats_norm = [norm(feat) for feat, norm in zip(hha_feats, self.hha_norms)]
 
-        fused0 = self.fusion_layers[0](torch.cat([rgb_feats_norm[1], hha_feats_norm[1]], dim=1))
+        fused1 = self.fusion_encoder.layer1(rgb_feats_norm[1] + hha_feats_norm[1])
 
-        fused1 = self.fusion_encoder.layer1(fused0)
-        fused1 = self.fusion_layers[1](torch.cat([rgb_feats_norm[2], hha_feats_norm[2], fused1], dim=1))
+        fused2 = self.fusion_encoder.fusion_conv1(fused1)
+        fused2 = self.fusion_encoder.layer2(fused2)
 
-        fused2 = self.fusion_encoder.layer2(fused1)
-        fused2 = self.fusion_layers[2](torch.cat([rgb_feats_norm[3], hha_feats_norm[3], fused2], dim=1))
+        fused3 = self.fusion_encoder.fusion_conv2(fused2)
+        fused3 = self.fusion_encoder.layer3(fused3)
 
-        fused3 = self.fusion_encoder.layer3(fused2)
-        fused3 = self.fusion_layers[3](torch.cat([rgb_feats_norm[4], hha_feats_norm[4], fused3], dim=1))
+        fused4 = self.fusion_encoder.fusion_conv3(fused3)
+        fused4 = self.fusion_encoder.layer4(fused4)
 
-        fused4 = self.fusion_encoder.layer4(fused3)
-        fused4 = self.fusion_layers[4](torch.cat([rgb_feats_norm[5], hha_feats_norm[5], fused4], dim=1))
+        fused5 = self.fusion_encoder.fusion_conv4(fused4)
 
-        output = self.decoder([rgb, fused0, fused1, fused2, fused3, fused4])
+        output = self.decoder([rgb, fused1, fused2, fused3, fused4, fused5])
         output = self.dropout(output) 
         return self.segmentation_head(output)
-    
+
     def get_optimizer_groups(self):
         rgb_encoder = [p for name, p in self.rgb_encoder.named_parameters() if "conv1" not in name]
         hha_encoder = [p for name, p in self.hha_encoder.named_parameters() if "conv1" not in name]
@@ -94,11 +90,11 @@ class UnetMidFusionHHA(nn.Module):
             {"params": self.hha_norms.parameters(), "lr": 1e-3},
             {"params": decoder, "lr": 1e-2},
         ]
-    
+
     def _adapt_input_channels(self):
         first_conv_rgb = self.rgb_encoder.conv1
         first_conv_hha = self.hha_encoder.conv1
-        
+
         new_conv_hha = nn.Conv2d(
             3,  # HHA channel
             out_channels=first_conv_hha.out_channels,
@@ -115,7 +111,7 @@ class UnetMidFusionHHA(nn.Module):
                 new_conv_hha.bias.data = first_conv_hha.bias.data.clone()
 
         self.hha_encoder.conv1 = new_conv_hha
-    
+
     def _create_bottleneck(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, dropout=0.0):
         return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
@@ -123,29 +119,38 @@ class UnetMidFusionHHA(nn.Module):
             nn.ReLU(inplace=True),
             nn.Dropout2d(p=dropout) if dropout > 0 else nn.Identity()
         )
-    
+
 
 class Encoder(nn.Module):
     def __init__(self, dropout=0.0):
         super().__init__()
-        self.layer1 = resnet.make_layer(resnet.Bottleneck, 64, blocks=3, planes=64, stride=2)
-        self.layer2 = resnet.make_layer(resnet.Bottleneck, 256, blocks=4, planes=128, stride=2)
-        self.layer3 = resnet.make_layer(resnet.Bottleneck, 512, blocks=6, planes=256, stride=2)
-        self.layer4 = resnet.make_layer(resnet.Bottleneck, 1024, blocks=3, planes=512, stride=2)
+        self.layer1 = resnet.make_layer(resnet.Bottleneck, 64, blocks=3, planes=16, stride=1)
+        self.fusion_conv1 = nn.Conv2d(64, 256, kernel_size=1, stride=2)
+        self.layer2 = resnet.make_layer(resnet.Bottleneck, 256, blocks=4, planes=64, stride=1)
+        self.fusion_conv2 = nn.Conv2d(256, 512, kernel_size=1, stride=2)
+        self.layer3 = resnet.make_layer(resnet.Bottleneck, 512, blocks=6, planes=128, stride=1)
+        self.fusion_conv3 = nn.Conv2d(512, 1024, kernel_size=1, stride=2)
+        self.layer4 = resnet.make_layer(resnet.Bottleneck, 1024, blocks=3, planes=256, stride=1)
+        self.fusion_conv4 = nn.Conv2d(1024, 2048, kernel_size=1, stride=2)
+        self.layer5 = resnet.make_layer(resnet.Bottleneck, 2048, blocks=3, planes=512, stride=1)
         self.dropout = nn.Dropout2d(p=dropout)
 
-        self.out_channels = [3, 64, 256, 512, 1024, 2048]
+        self.out_channels = [64, 256, 512, 1024, 2048]
     
     def forward(self, x):
         out = [x]
         x = self.layer1(x)
         out.append(x)
+        x = self.fusion_conv1(x)
         x = self.layer2(x)
         out.append(x)
+        x = self.fusion_conv2(x)
         x = self.layer3(x)
         out.append(x)
+        x = self.fusion_conv3(x)
         x = self.layer4(x)
         out.append(x)
+        x = self.fusion_conv4(x)
         x = self.layer5(x)
         x = self.dropout(x)
         out.append(x)
