06300ee61a1951a3428264ad9a3ff6b1889cba63
Marco Túlio P. T. Tristão <marcotuliopin@gmail.com>
2025-06-25 21:50:51 -0300

    refactor: remove unused ResNet50 decoder and related scripts

--- scripts/install_nyu_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p data/NYUDepthv2_seg
-
-git clone --depth 1 https://huggingface.co/datasets/wyrx/NYUDepthv2_seg data/nyuv2/old
-rm -rf data/nyuv2/old/.git data/nyuv2/old/.gitattributes
-
-mv data/nyuv2/old/data/train-00000-of-00001.parquet data/nyuv2/data/train.parquet
-mv data/nyuv2/old/data/test-00000-of-00001.parquet data/nyuv2/data/test.parquet
